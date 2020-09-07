cd914b4e59aed6d2f9095e0364168ca099d53966
NISHAPURSWANI <43604816+NISHAPURSWANI@users.noreply.github.com>
2020-09-07 15:59:00 +0530

    update in modal & card component

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,25 +1,22 @@
 <template>
-<div>
-  <div class="board">
+  <div>
+    <div class="board">
       <boards></boards>
       <between></between>
- </div>
-     <br>
-     <div class="mylist">
+    </div>
+    <br />
+    <div class="mylist">
       <list></list>
       <card></card>
-      </div>
-
-      
-      
- </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import Boards from "@/components/Boards.vue";
 import Between from "@/components/Between.vue";
 import List from "@/components/List.vue";
-import Card from "@/components/Card.vue"
+import Card from "@/components/Card.vue";
 export default {
   name: "Board",
   components: {
@@ -27,33 +24,23 @@ export default {
     Between,
     List,
     Card
-    
-    
   }
 };
 </script>
 1<style >
-.boards{
-
-
-
-background-image: url("https://i.imgur.com/7AqxKzf.jpg");
+.boards {
+  background-image: url("https://i.imgur.com/7AqxKzf.jpg");
   background-size: cover;
   background-attachment: fixed;
 }
-.mylist{
-  min-height:500px;
-  max-width:300px;
+.mylist {
+  min-height: 500px;
+  max-width: 300px;
   background-color: rgba(245, 241, 241, 0.904);
   border-radius: 4px;
-  align-self: center ;
-  /*overflow-x: auto;*/
- /*overflow-y: auto;*/
-  text-align:center;
-  /*justify-content: center;*/
- /* align-content: center;*/
- margin-left: 50px;
- margin-bottom:px;
+  align-self: center;
+  text-align: center;
+  margin-left: 50px;
+  margin-bottom: px;
 }
-
 </style>
