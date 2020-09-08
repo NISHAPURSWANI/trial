6255ad94385528afe954a41246f81897dc0adf3f
NISHAPURSWANI <43604816+NISHAPURSWANI@users.noreply.github.com>
2020-09-08 22:01:40 +0530

    spinner and api valdation

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,79 +6,108 @@
     <br />
     <br />
 
-    <div class="form">
+    <div class="form" id="loginform">
       <b-card>
-      <form>
-        <b>Login to Trello</b>
-        <br />
-        <br />
-        <label for="username">
+        <form v-on:submit.prevent="submit">
+          <b>Login to Trello</b>
+          <br />
+          <br />
           <input
             type="text"
-            v-model="username"
+            v-model.trim="$v.username.$model"
+            :class="{'is-invalid': validationStatus($v.username)}"
+            class="form-control form-control-lg"
             placeholder="Enter your username"
-            style="height:50px; width:350px"
+            style="height:50px; width:350px; margin-left:70px;"
           />
-        </label>
-        <br />
-        <br />
-        <label for="password">
+          <div
+            v-if="!$v.username.required"
+            class="invalid-feedback"
+          >The user name field is required.</div>
+
+          <br />
+          <br />
           <input
             type="password"
-            v-model="password"
+            v-model.trim="$v.password.$model"
+            :class="{'is-invalid': validationStatus($v.password)}"
+            class="form-control form-control-lg"
             placeholder="Enter your password"
-            style="height:50px; width:350px"
+            style="height:50px; width:350px;margin-left:70px;"
+            @keypress.enter="validationStatus()"
           />
-        </label>
-        <br />
-        <br />
-        <router-link to="/board">
+          <div v-if="!$v.password.required" class="invalid-feedback">The password field is required.</div>
+          <br />
+          <div v-show="spin">
+            <b-spinner variant="primary" label="Spinning"></b-spinner>
+          </div>
+
           <label for="login">
-            <button class="login" @click='verify()'>Login</button>
+            <button class="login" @click="spin=true">Login</button>
           </label>
           <br />
-        </router-link>
-      </form>
+        </form>
       </b-card>
     </div>
+
+    <br />
+    <br />
   </div>
 </template>
 <script>
 import Vue from "vue";
-import axios from 'axios'
-import VueAxios from 'vue-axios'
- Vue.use(VueAxios, axios)
+import axios from "axios";
+import VueAxios from "vue-axios";
+import { required } from "vuelidate/lib/validators";
+
+Vue.use(VueAxios, axios);
 
 export default {
-  name: "HelloWorld",
-     
-    
+  name: "Login",
+
   data: function() {
     return {
-      email: "",
       username: "",
-      password: ""
+      password: "",
+      spin: false
     };
-    
   },
-  methods:{
+  validations: {
+    username: { required },
 
-    verify(){
-      axios.post('https://trello-clone-123.herokuapp.com/rest-auth/login/',{
-        username:this.username,
-        password:this.password
-        
-      })
-      .then((response)=>{
-        console.log(response);  
-
-      })
+    password: { required }
+  },
+  methods: {
+    validationStatus: function(validation) {
+      return typeof validation != "undefined" ? validation.$error : false;
+    },
+    submit: function() {
+      this.$v.$touch();
+      if (this.$v.$pendding || this.$v.$error) {
+                    this.spin = false;
+                    return;
+      }
+      else {
+        axios.post("https://trello-clone-123.herokuapp.com/rest-auth/login/", {
+            username: this.username,
+            password: this.password
+          })
+          .then(response => {
+            if (response.status === 200) {
+              this.spin = false;
+              console.log(response);
+              this.$router.push({ name: "Board" });
+            } else if(response.statuscode===400) {
+              alert("invalid creditenials");
+              this.spin = false;
+              alert("invalid ");
+            }
+          });
+      }
     }
-    
-
   }
-}
-  </script>
+};
+</script>
 
   
 
@@ -101,9 +130,8 @@ export default {
 .formsec {
   justify-content: center;
   background-color: white;
-  height:auto;
-  width:auto;
-
+  height: auto;
+  width: auto;
 }
 .image {
   width: 550px;
--- src/main.js
+++ src/main.js
@@ -7,12 +7,17 @@ import router from "./router";
 import store from "./store";
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import axios from 'axios';
+
+import VueAxios from 'vue-axios';
+import Vuelidate from 'vuelidate';
+
  
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios)
+Vue.use(Vuelidate);
+
 
 
 new Vue({
