d7f7cc7f3f7347deae03880f56369b4fcdb5ee88
NISHAPURSWANI <2016.nisha.purswani@ves.ac.in>
2020-09-14 11:07:03 +0530

    routing changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,70 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+//import Home from "../views/Home.vue";
+//import { toPrecision } from "core-js/fn/number/epsilon";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+   // name: "Home",
+    //component: Home,
+    redirect: "/board",
+
   },
   {
     path: "/about",
     name: "About",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      //import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+
   },
   {
     path: "/login",
     name: "Login",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../components/Login.vue")
+      //import(/* webpackChunkName: "login" */ "../components/Login.vue"),
+      import(/* webpackChunkName: "login" */ "../components/Login.vue"),
+
+    meta:{requiresAuth: false }
+
   },
   {
     path: "/board",
     name: "Board",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../components/Board.vue")
-  },
-  
+      import(/* webpackChunkName: "board" */ "../components/Board.vue"),
+      meta: { requiresAuth: true },
+    },
+
 ];
-
-
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
+
+
   routes
 });
 
-export default router;
+router.beforeEach((to,from,next)=>{
+
+  let token=localStorage.getItem("TOKEN")
+  if(to.name==="Board"){
+    if(token){
+      console.log(token)
+      next()
+    }
+    else{
+      next('/login')
+    }
+  }
+  else{
+    next('/login')
+  }
+})
+export default router
+
+
